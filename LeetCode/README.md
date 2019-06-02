0516005080d541ff3fd3f3c8933724a64b502a77
Michael Williams <mjwilli8@umd.edu>
2019-06-02 01:52:15 -0400

    completed longest common prefix

--- LeetCode/README.md
+++ LeetCode/README.md
@@ -4,5 +4,7 @@
 * [7. Reverse Integer](https://leetcode.com/problems/reverse-integer/)
 * [9. Palindrome Number](https://leetcode.com/problems/palindrome-number/)
 * [13. Roman to Integer](https://leetcode.com/problems/roman-to-integer/)
+* [14. Longest Common
+* Prefix](https://leetcode.com/problems/longest-common-prefix/)
 ## Medium
 ## Hard
---
+++ LeetCode/Solutions/#14/solution.py
@@ -0,0 +1,31 @@
+class Solution:
+    def longestCommonPrefix(self, strs: List[str]) -> str:
+        # Special cases
+        strListLen = len(strs)
+        if strListLen == 0:
+            return ""
+        elif strListLen == 1:
+            return strs[0]
+        
+        firstStr = strs[0]
+        secondStr = strs[1]
+        secondStrLen = len(secondStr)
+        
+        end = -1
+        count = -1
+        
+        for counter,value in enumerate(firstStr):
+            if counter >= secondStrLen or value != secondStr[counter]:
+                end = counter
+                break
+            count = counter
+        if count != -1:
+            if end == -1:
+                end = secondStrLen - 1
+            for i in range(0,end+1):
+                for s in range(0,strListLen-1):
+                    if i >= len(strs[s]) or i >= len(strs[s + 1]) or strs[s][i] != strs[s + 1][i]:
+                        return secondStr[:i]
+            return firstStr
+        else:
+            return ""
