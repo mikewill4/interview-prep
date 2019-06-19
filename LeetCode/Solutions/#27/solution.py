0e217079144467bd6f2de1c166a01740f781ee45
Michael Williams <mjwilli8@umd.edu>
2019-06-18 22:36:45 -0400

    completed remove element

---
+++ LeetCode/Solutions/#27/solution.py
@@ -0,0 +1,10 @@
+class Solution:
+    def removeElement(self, nums: List[int], val: int) -> int:
+        length = len(nums)
+        ptrOne = ptrTwo = 0
+        while ptrTwo < length:
+            if nums[ptrTwo] != val:
+                nums[ptrOne] = nums[ptrTwo]
+                ptrOne += 1
+            ptrTwo += 1
+        return ptrOne
