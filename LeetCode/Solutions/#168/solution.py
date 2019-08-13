b07571da7eb6cf440b54a538d42402e45110705c
Michael Williams <mjwilli8@umd.edu>
2019-08-13 10:22:00 -0400

    fixed convert to title

--- LeetCode/Solutions/#168/solution.py
+++ LeetCode/Solutions/#168/solution.py
@@ -1,10 +1,13 @@
 class Solution:
-	def convertToTitle(self, n: int) -> str:
-		output_str = ""
-		while n > 26:
-			char_n = chr(n % 26 + 64)
-			output_str = char_n + output_str
-			n = n // 26
-		char_n = chr(n + 64)
-		output_str = char_n + output_str
-		return output_str
+    def convertToTitle(self, n: int) -> str:
+        output_str = ""
+        while n > 26:
+            char_n = chr(n % 26 + 64)
+            if char_n == "@":
+                char_n = "Z"
+                n -= 1
+            output_str = char_n + output_str
+            n = n // 26
+        char_n = chr(n + 64)
+        output_str = char_n + output_str
+        return output_str
